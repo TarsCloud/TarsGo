15b2599104bfc771533d9f218068c07d3fa4e910
sandyskies <chenmingjie0828@163.com>
2018-09-18 20:37:49 +0800

    change testing info

--- tars/util/endpoint/parse_test.go
+++ tars/util/endpoint/parse_test.go
@@ -6,9 +6,9 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	e := Parse("tcp -h 10.219.139.142 -p 19386 -t 60000")
+	e := Parse("tcp -h 127.0.0.1 -p 19386 -t 60000")
 	fmt.Println(e)
-	e2 := Parse("udp -h 10.219.139.142 -p 19386 -t 60000")
+	e2 := Parse("udp -h 127.0.0.1 -p 19386 -t 60000")
 	fmt.Println(e2)
 	tars := Endpoint2tars(e2)
 	fmt.Println(tars)
