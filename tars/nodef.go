6292e1140a43c2a0d2afc45655f32dc66a8ee7ee
jiechaowu(吴杰超) <jiechaowu@tencent.com>
2018-10-19 10:18:36 +0800

    Solved golint problems for tars/nodef.go

--- tars/nodef.go
+++ tars/nodef.go
@@ -5,12 +5,15 @@ import (
 	"os"
 )
 
+// NodeFHelper struct
 type NodeFHelper struct {
 	comm *Communicator
 	si   nodef.ServerInfo
 	sf   *nodef.ServerF
 }
 
+// SetNodeInfo sets node info
+// with Communicator comm, string node name, string app name, string server
 func (n *NodeFHelper) SetNodeInfo(comm *Communicator, node string, app string, server string) {
 	n.comm = comm
 	n.sf = new(nodef.ServerF)
@@ -25,6 +28,7 @@ func (n *NodeFHelper) SetNodeInfo(comm *Communicator, node string, app string, s
 	}
 }
 
+// KeepAlive keeps the NodeFHelper's ServerF alive
 func (n *NodeFHelper) KeepAlive(adapter string) {
 	n.si.Adapter = adapter
 	_, err := n.sf.KeepAlive(&n.si)
@@ -33,6 +37,7 @@ func (n *NodeFHelper) KeepAlive(adapter string) {
 	}
 }
 
+// ReportVersion reports the version with string verion
 func (n *NodeFHelper) ReportVersion(version string) {
 	_, err := n.sf.ReportVersion(n.si.Application, n.si.ServerName, version)
 	if err != nil {
