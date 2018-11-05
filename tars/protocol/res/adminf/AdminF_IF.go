1c7ee326b9519950e7a3a2b2171a3ff90d58eb65
Chen Mingjie <chenmingjie0828@163.com>
2018-11-05 22:04:39 +0800

    remake res

--- tars/protocol/res/adminf/AdminF_IF.go
+++ tars/protocol/res/adminf/AdminF_IF.go
@@ -9,6 +9,7 @@ import (
 	m "github.com/TarsCloud/TarsGo/tars/model"
 	"github.com/TarsCloud/TarsGo/tars/protocol/codec"
 	"github.com/TarsCloud/TarsGo/tars/protocol/res/requestf"
+	"github.com/TarsCloud/TarsGo/tars/util/current"
 	"github.com/TarsCloud/TarsGo/tars/util/tools"
 )
 
@@ -24,8 +25,15 @@ func (_obj *AdminF) Shutdown(_opt ...map[string]string) (err error) {
 	var have bool
 	var ty byte
 	_os := codec.NewBuffer()
+
 	var _status map[string]string
 	var _context map[string]string
+	if len(_opt) == 1 {
+		_context = _opt[0]
+	} else if len(_opt) == 2 {
+		_context = _opt[0]
+		_status = _opt[1]
+	}
 	_resp := new(requestf.ResponsePacket)
 	ctx := context.Background()
 	err = _obj.s.Tars_invoke(ctx, 0, "shutdown", _os.ToBytes(), _status, _context, _resp)
@@ -33,6 +41,28 @@ func (_obj *AdminF) Shutdown(_opt ...map[string]string) (err error) {
 		return err
 	}
 
+	if len(_opt) == 1 {
+		for k, _ := range _context {
+			delete(_context, k)
+		}
+		for k, v := range _resp.Context {
+			_context[k] = v
+		}
+	} else if len(_opt) == 2 {
+		for k, _ := range _context {
+			delete(_context, k)
+		}
+		for k, v := range _resp.Context {
+			_context[k] = v
+		}
+		for k, _ := range _status {
+			delete(_status, k)
+		}
+		for k, v := range _resp.Status {
+			_status[k] = v
+		}
+
+	}
 	_ = length
 	_ = have
 	_ = ty
@@ -48,12 +78,40 @@ func (_obj *AdminF) ShutdownWithContext(ctx context.Context, _opt ...map[string]
 	_os := codec.NewBuffer()
 	var _status map[string]string
 	var _context map[string]string
+	if len(_opt) == 1 {
+		_context = _opt[0]
+	} else if len(_opt) == 2 {
+		_context = _opt[0]
+		_status = _opt[1]
+	}
 	_resp := new(requestf.ResponsePacket)
 	err = _obj.s.Tars_invoke(ctx, 0, "shutdown", _os.ToBytes(), _status, _context, _resp)
 	if err != nil {
 		return err
 	}
 
+	if len(_opt) == 1 {
+		for k, _ := range _context {
+			delete(_context, k)
+		}
+		for k, v := range _resp.Context {
+			_context[k] = v
+		}
+	} else if len(_opt) == 2 {
+		for k, _ := range _context {
+			delete(_context, k)
+		}
+		for k, v := range _resp.Context {
+			_context[k] = v
+		}
+		for k, _ := range _status {
+			delete(_status, k)
+		}
+		for k, v := range _resp.Status {
+			_status[k] = v
+		}
+
+	}
 	_ = length
 	_ = have
 	_ = ty
@@ -74,6 +132,12 @@ func (_obj *AdminF) Notify(Command string, _opt ...map[string]string) (ret strin
 
 	var _status map[string]string
 	var _context map[string]string
+	if len(_opt) == 1 {
+		_context = _opt[0]
+	} else if len(_opt) == 2 {
+		_context = _opt[0]
+		_status = _opt[1]
+	}
 	_resp := new(requestf.ResponsePacket)
 	ctx := context.Background()
 	err = _obj.s.Tars_invoke(ctx, 0, "notify", _os.ToBytes(), _status, _context, _resp)
@@ -86,6 +150,28 @@ func (_obj *AdminF) Notify(Command string, _opt ...map[string]string) (ret strin
 		return ret, err
 	}
 
+	if len(_opt) == 1 {
+		for k, _ := range _context {
+			delete(_context, k)
+		}
+		for k, v := range _resp.Context {
+			_context[k] = v
+		}
+	} else if len(_opt) == 2 {
+		for k, _ := range _context {
+			delete(_context, k)
+		}
+		for k, v := range _resp.Context {
+			_context[k] = v
+		}
+		for k, _ := range _status {
+			delete(_status, k)
+		}
+		for k, v := range _resp.Status {
+			_status[k] = v
+		}
+
+	}
 	_ = length
 	_ = have
 	_ = ty
@@ -106,6 +192,12 @@ func (_obj *AdminF) NotifyWithContext(ctx context.Context, Command string, _opt 
 
 	var _status map[string]string
 	var _context map[string]string
+	if len(_opt) == 1 {
+		_context = _opt[0]
+	} else if len(_opt) == 2 {
+		_context = _opt[0]
+		_status = _opt[1]
+	}
 	_resp := new(requestf.ResponsePacket)
 	err = _obj.s.Tars_invoke(ctx, 0, "notify", _os.ToBytes(), _status, _context, _resp)
 	if err != nil {
@@ -117,6 +209,28 @@ func (_obj *AdminF) NotifyWithContext(ctx context.Context, Command string, _opt 
 		return ret, err
 	}
 
+	if len(_opt) == 1 {
+		for k, _ := range _context {
+			delete(_context, k)
+		}
+		for k, v := range _resp.Context {
+			_context[k] = v
+		}
+	} else if len(_opt) == 2 {
+		for k, _ := range _context {
+			delete(_context, k)
+		}
+		for k, v := range _resp.Context {
+			_context[k] = v
+		}
+		for k, _ := range _status {
+			delete(_status, k)
+		}
+		for k, v := range _resp.Status {
+			_status[k] = v
+		}
+
+	}
 	_ = length
 	_ = have
 	_ = ty
@@ -197,7 +311,16 @@ func (_obj *AdminF) Dispatch(ctx context.Context, _val interface{}, req *request
 	default:
 		return fmt.Errorf("func mismatch")
 	}
-	var status map[string]string
+	var _status map[string]string
+	s, ok := current.GetResponseStatus(ctx)
+	if ok && s != nil {
+		_status = s
+	}
+	var _context map[string]string
+	c, ok := current.GetResponseContext(ctx)
+	if ok && c != nil {
+		_context = c
+	}
 	*resp = requestf.ResponsePacket{
 		IVersion:     1,
 		CPacketType:  0,
@@ -205,9 +328,9 @@ func (_obj *AdminF) Dispatch(ctx context.Context, _val interface{}, req *request
 		IMessageType: 0,
 		IRet:         0,
 		SBuffer:      tools.ByteToInt8(_os.ToBytes()),
-		Status:       status,
+		Status:       _status,
 		SResultDesc:  "",
-		Context:      req.Context,
+		Context:      _context,
 	}
 	_ = length
 	_ = have
