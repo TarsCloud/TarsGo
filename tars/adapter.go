0f0b2fbd5af7011ad8535c5f3ad582a2461e6391
Chen Mingjie <chenmingjie0828@163.com>
2018-11-16 21:09:13 +0800

    Revert "Dev (#71)"
    This reverts commit ee912715a32f59c7d84356007cb39ded9acac1be.

--- tars/adapter.go
+++ tars/adapter.go
@@ -55,6 +55,13 @@ func (c *AdapterProxy) ParsePackage(buff []byte) (int, int) {
 
 // Recv : Recover read channel when closed for timeout
 func (c *AdapterProxy) Recv(pkg []byte) {
+	defer func() {
+		// TODO readCh has a certain probability to be closed after the load, and we need to recover
+		// Maybe there is a better way
+		if err := recover(); err != nil {
+			TLOG.Error("recv pkg painc:", err)
+		}
+	}()
 	packet := requestf.ResponsePacket{}
 	err := packet.ReadFrom(codec.NewReader(pkg))
 	if err != nil {
@@ -65,11 +72,7 @@ func (c *AdapterProxy) Recv(pkg []byte) {
 	if ok {
 		ch := chIF.(chan *requestf.ResponsePacket)
 		TLOG.Debug("IN:", packet)
-		select {
-		case ch <- &packet:
-		default:
-			close(ch)
-		}
+		ch <- &packet
 	} else {
 		TLOG.Error("timeout resp,drop it:", packet.IRequestId)
 	}
