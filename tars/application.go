5beaebd228aff07c76c61b9075fa50ad118d4e33
jonathanmin <jonathanmin@tencent.com>
2019-05-03 00:20:32 +0800

    exit when config is nil.avoid user hold nil config

--- tars/application.go
+++ tars/application.go
@@ -53,11 +53,13 @@ func initConfig() {
 	confPath := flag.String("config", "", "init config path")
 	flag.Parse()
 	if len(*confPath) == 0 {
+		os.Exit(1)
 		return
 	}
 	c, err := conf.NewConf(*confPath)
 	if err != nil {
 		TLOG.Error("open app config fail")
+		os.Exit(1)
 	}
 	//Config.go
 	//Server
