1ce537af1cd9938480a43d1af1cd8724475ffd43
yifabao <yifabao@gmail.com>
2020-07-20 13:10:44 +0800

    resolve conflicts

--- tars/tools/tars2go/gen_go.go
+++ tars/tools/tars2go/gen_go.go
@@ -763,7 +763,6 @@ if ty == codec.LIST {
 		c.WriteString("}\n")
 	}
 }
-`)
 
 func (gen *GenGo) genReadArray(mb *StructMember, prefix string, hasRet bool) {
 	c := &gen.code
@@ -1554,23 +1553,13 @@ func (gen *GenGo) genSwitchCase(tname string, fun *FunInfo) {
 	if tarsReq.IVersion == basef.TARSVERSION {
 	_os.Reset()
 	`)
-
-//	if fun.HasRet {
-//		c.WriteString(`
-//		err = _os.Write_int32(_funRet_, 0)
-//		if err != nil {
-//			return err
-//		}
-//`)
-//	}
-
 	if fun.HasRet {
-		dummy := &StructMember{}
-		dummy.Type = fun.RetType
-		dummy.Key = "_funRet_"
-		dummy.Tag = 0
-		dummy.Require = true
-		gen.genWriteVar(dummy, "", false)
+		c.WriteString(`
+		err = _os.Write_int32(_funRet_, 0)
+		if err != nil {
+			return err
+		}		
+`)
 	}
 
 	for k, v := range fun.Args {
@@ -1589,27 +1578,13 @@ func (gen *GenGo) genSwitchCase(tname string, fun *FunInfo) {
 _tupRsp_ := tup.NewUniAttribute()
 `)
 
-//	if fun.HasRet {
-//		c.WriteString(`
-//		_os.Reset()
-//		err = _os.Write_int32(_funRet_, 0)
-//		if err != nil {
-//			return err
-//		}
-//		_tupRsp_.PutBuffer("", _os.ToBytes())
-//		_tupRsp_.PutBuffer("tars_ret", _os.ToBytes())
-//`)
-//	}
-
 	if fun.HasRet {
-		dummy := &StructMember{}
-		dummy.Type = fun.RetType
-		dummy.Key = "_funRet_"
-		dummy.Tag = 0
-		dummy.Require = true
-		gen.genWriteVar(dummy, "", false)
-
 		c.WriteString(`
+		_os.Reset()
+		err = _os.Write_int32(_funRet_, 0)
+		if err != nil {
+			return err
+		}
 		_tupRsp_.PutBuffer("", _os.ToBytes())
 		_tupRsp_.PutBuffer("tars_ret", _os.ToBytes())
 `)
