826f60ab0de996e61b3c12ab0babb4f8947d7a99
liubingbing2 <liubingbing2@tal.com>
2022-01-10 23:08:32 +0800

    优化获取本机ip逻辑

--- tars/util/tools/ip.go
+++ tars/util/tools/ip.go
@@ -9,19 +9,20 @@ func init() {
 }
 
 func GetLocalIP() string {
-	if cacheIP == "" {
-		conn, err := net.Dial("udp", "10.0.0.1:80")
-		if err != nil {
-			return "0.0.0.0"
-		}
-		defer conn.Close()
+	if cacheIP != "" {
+		return cacheIP
+	}
+	conn, err := net.Dial("udp", "10.0.0.1:80")
+	if err != nil {
+		return "0.0.0.0"
+	}
+	defer conn.Close()
 
-		addr := conn.LocalAddr().(*net.UDPAddr)
-		if addr == nil {
-			cacheIP = "0.0.0.0"
-		} else {
-			cacheIP = addr.IP.String()
-		}
+	addr := conn.LocalAddr().(*net.UDPAddr)
+	if addr == nil {
+		cacheIP = "0.0.0.0"
+	} else {
+		cacheIP = addr.IP.String()
 	}
 	return cacheIP
 }
