a10adfe71bb90f97b8590cdf345004a9383c2617
lbbniu <lbbniu@gmail.com>
2022-01-26 12:29:26 +0800

    修改测试用例

--- tars/tools/tarsgo/internal/base/repo_test.go
+++ tars/tools/tarsgo/internal/base/repo_test.go
@@ -6,11 +6,16 @@ import (
 )
 
 func TestRepo(t *testing.T) {
-	r := NewRepo("https://github.com/TarsCloud/TarsGo.git", "")
+	r := NewRepo("https://github.com/TarsCloud/TarsGo.git", "", "")
 	if err := r.Clone(context.Background()); err != nil {
 		t.Fatal(err)
 	}
-	if err := r.CopyTo(context.Background(), "/tmp/test_repo", "github.com/TarsCloud/TarsGo", nil); err != nil {
+	if err := r.CopyTo(context.Background(), &Project{
+		App:          "DemoApp",
+		Server:       "DemoServer",
+		Servant:      "DemoServant",
+		GoModuleName: "demo",
+	}, "github.com/TarsCloud/TarsGo", "", []string{}); err != nil {
 		t.Fatal(err)
 	}
 }
