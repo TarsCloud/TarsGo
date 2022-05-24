88260916ac3a393f06df9d003468066347334339
lbbniu <lbbniu@gmail.com>
2022-05-24 15:34:26 +0800

    update tarsgo tools version

--- tars/tools/tarsgo/internal/consts/version.go
+++ tars/tools/tarsgo/internal/consts/version.go
@@ -3,7 +3,7 @@ package consts
 // Release is the current tarsgo tool version.
 const (
 	RepoURL      = "https://github.com/TarsCloud/TarsGo.git"
-	Release      = "v1.2.0"
+	Release      = "v1.3.3"
 	MakeDemoDir  = "Demo4GoMod"
 	CMakeDemoDir = "Demo4Cmake"
 	Make         = "make"
